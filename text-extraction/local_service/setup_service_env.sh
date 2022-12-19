292923bf1d826f8ffbbfe9b036ec92632ca6a7b0
orionsBeltWest <basem.barakat@intel.com>
2022-12-19 20:43:44 +0000

    Added env and and started scripts to ocr service

---
+++ text-extraction/local_service/setup_service_env.sh
@@ -0,0 +1,7 @@
+#export HOME_ROOT="/root/text-extraction"
+export HOME_ROOT="/bbarakat/wrk"
+export PTHONPATH=${HOME_ROOT}:"${HOME_ROOT}/infrance":"${HOME_ROOT}/patch":"${HOME_ROOT}/local_service/local_grpc"
+
+export LOCAL_SERVER_PORT=50051
+export S3_SERVICE_ADDRESS="172.17.0.3"
+
