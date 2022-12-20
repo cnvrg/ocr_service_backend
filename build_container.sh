b20bb539db9a57875a4d0be77bea07382cbe9842
orionsBeltWest <basem.barakat@intel.com>
2022-12-20 23:11:41 +0000

    adding client and data directory

---
+++ build_container.sh
@@ -0,0 +1,7 @@
+local_tag=${2:-"ubuntu_20.04_MVP_Set1"}
+Dockerfile_name=${1:-"Dockerfile"}
+
+Container_image_local="lamatriz/wlpu:${local_tag}"
+
+
+docker build -t ${Container_image_local}  -f ${Dockerfile_name} .
