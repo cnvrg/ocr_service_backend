a44720f462b8bd080deeb70aff67b10f364ce296
orionsBeltWest <basem.barakat@intel.com>
2023-01-11 05:13:53 +0000

    Added go install to Dockerfile removed it from build script

--- build_ocr_container.sh
+++ build_ocr_container.sh
@@ -3,15 +3,6 @@ Dockerfile_name=${1:-"Dockerfile_ocr"}
 
 Container_image_local="lamatriz/wlpu:${local_tag}"
 
-## Download to be installed in docker file (Copy go)
-#wget "https://go.dev/dl/go1.19.5.linux-amd64.tar.gz" 
-
-
-#tar -xf go1.19.5.linux-amd64.tar.gz
-
-# temp workarround for wget issue 
-[ ! -d "go" ] && echo "making dir go" && mkdir go
-
-
+# build container and push it to repo
 docker build -t ${Container_image_local}  -f ${Dockerfile_name} .
 docker push ${Container_image_local}
